0207cfb1fedcdeb7027cb895c49e3f918bc73e0c
Iago Rampazzo <iagorampazzo@hotmail.com>
2014-07-07 18:51:29 -0300

    Recebendo mensagens do Cliente
    Recebe mensagens do cliente para andar com o item selecionado:
    "Direita", "Esquerda", "Baixo" e "Cima"

---
+++ node-webkit/funcoes.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	var styles = {
+      backgroundColor : "#ddd",
+      fontWeight: ""
+    };
+
+	$('h2').fadeOut(1000);
+	$('#x3y0').click(function(){
+		$(this).fadeOut(2000);
+	});
+});
